95f5e462363f51e3746b8a3e9185c1dd460f3474
Lucas Deng <101198386+Lucasdenggithub@users.noreply.github.com>
2022-05-02 22:43:47 +0800

    Add files via upload

---
+++ Dolphin.java
@@ -0,0 +1,28 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Dolphin here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Dolphin extends Actor
+{
+    /**
+     * Act - do whatever the Dolphin wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    
+    public void act()
+    {
+        if(Greenfoot.isKeyDown("a"))
+        {
+            move(-2);
+        }
+        if(Greenfoot.isKeyDown("d"))
+        {
+            move(2);
+        }
+        // Add your action code here.
+    }
+}
