8f5780802009fc0490a7a1e9d20adc38a145ef90
Lucas Deng <349939298@gapps.yrdsb.ca>
2022-05-04 22:34:55 +0800

    fish drop

--- Dolphin.java
+++ Dolphin.java
@@ -12,7 +12,6 @@ public class Dolphin extends Actor
      * Act - do whatever the Dolphin wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
-    
     public void act()
     {
         if(Greenfoot.isKeyDown("a"))
