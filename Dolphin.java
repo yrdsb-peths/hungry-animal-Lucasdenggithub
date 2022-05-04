afb49be647e50d76410bcd39e47b54a42b76f87f
Lucas Deng <101198386+Lucasdenggithub@users.noreply.github.com>
2022-05-04 22:24:28 +0800

    Add files via upload

--- Dolphin.java
+++ Dolphin.java
@@ -1,3 +1,44 @@
+<<<<<<< HEAD
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Dolphin here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Dolphin extends Actor
+{
+    /**
+     * Act - do whatever the Dolphin wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    
+    public void act()
+    {
+        if(Greenfoot.isKeyDown("a"))
+        {
+            move(-2);
+        }
+        if(Greenfoot.isKeyDown("d"))
+        {
+            move(2);
+        }
+        
+        eat();
+    }
+    
+    public void eat()
+    {
+        if(isTouching(Fish.class))
+        {
+            removeTouching(Fish.class);
+            MyWorld world = (MyWorld) getWorld();
+            world.spawnFish();
+        }
+    }
+}
+=======
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
@@ -30,3 +71,4 @@ public class Dolphin extends Actor
         }// Add your action code here.
     }
 }
+>>>>>>> 4b201fe2bc2781793bb9f64f19b298344f42b789
--- MyWorld.java
+++ MyWorld.java
@@ -1,3 +1,39 @@
+<<<<<<< HEAD
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class MyWorld here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class MyWorld extends World
+{
+
+    /**
+     * Constructor for objects of class MyWorld.
+     * 
+     */
+    public MyWorld()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+        Dolphin d = new Dolphin();
+        addObject(d, 300, 350);
+        
+        spawnFish();
+
+    }
+    
+    public void spawnFish()
+    {
+        int x = Greenfoot.getRandomNumber(600);
+        int y = 0;
+        Fish fish = new Fish();
+        addObject(fish,x, y);
+    }
+}
+=======
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
@@ -25,3 +61,4 @@ public class MyWorld extends World
         addObject(fish,300, 0);
     }
 }
+>>>>>>> 4b201fe2bc2781793bb9f64f19b298344f42b789
