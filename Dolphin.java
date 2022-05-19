924df42f997922d2a6f345afee67a1b4e037818d
Lucas Deng <101198386+Lucasdenggithub@users.noreply.github.com>
2022-05-19 09:33:14 +0800

    Add files via upload

--- Dolphin.java
+++ Dolphin.java
@@ -12,22 +12,50 @@ public class Dolphin extends Actor
      * Act - do whatever the Dolphin wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
-    GreenfootImage[] idle = new GreenfootImage[6];
+    GreenfootImage[] idleRight = new GreenfootImage[6];
+    GreenfootImage[] idleLeft = new GreenfootImage[6];
+    String facing = "right";
+    SimpleTimer animationTimer = new SimpleTimer();
     
     public Dolphin()
     {
-        for(int i = 0; i < idle.length; i++)
+        for(int i = 0; i < idleRight.length; i++)
         {
-            idle[i] = new GreenfootImage("images/dolphin_idle/idle" + i + ".png.png");
+            idleRight[i] = new GreenfootImage("images/dolphin_idle/idle" + i + ".png.png");
+            idleRight[i].scale(150,150);
         }
-        setImage(idle[0]);
+        
+        for(int i = 0; i < idleLeft.length; i++)
+        {
+            idleLeft[i] = new GreenfootImage("images/dolphin_idle/idle" + i + ".png.png");
+            idleLeft[i].mirrorHorizontally(); 
+            idleLeft[i].scale(150,150); 
+        }
+        
+        animationTimer.mark();
+        
+        setImage(idleRight[0]);
     }
     
     int imageIndex = 0;
     public void animateDolphin()
     {
-        setImage(idle[imageIndex]);
-        imageIndex = (imageIndex + 1) % idle.length;
+        if(animationTimer.millisElapsed() < 200)
+        {
+            return;
+        }
+        animationTimer.mark();
+        
+        if(facing.equals("right"))
+        {
+            setImage(idleRight[imageIndex]);
+            imageIndex = (imageIndex + 1) % idleRight.length;
+        }
+        else
+        {
+            setImage(idleLeft[imageIndex]);
+            imageIndex = (imageIndex + 1) % idleLeft.length;
+        }
         
     }
     
@@ -37,10 +65,12 @@ public class Dolphin extends Actor
         if(Greenfoot.isKeyDown("a"))
         {
             move(-2);
+            facing = "left";
         }
         if(Greenfoot.isKeyDown("d"))
         {
             move(2);
+            facing = "right";
         }
         
         eat();
