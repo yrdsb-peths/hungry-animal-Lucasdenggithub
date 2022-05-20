1ffbd47472e7400d218c7d00dfea2f2806ea3f41
Lucas Deng <101198386+Lucasdenggithub@users.noreply.github.com>
2022-05-20 22:37:29 +0800

    Add files via upload

--- Fish.java
+++ Fish.java
@@ -2,10 +2,12 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 public class Fish extends Actor
 {
+    
+    int speed = 1;
     public void act()
     {
         int x = getX();
-        int y = getY();
+        int y = getY() + speed;
         setLocation(x, y+2);
         
         MyWorld world = (MyWorld) getWorld();
@@ -14,5 +16,10 @@ public class Fish extends Actor
             world.gameOver();
             world.removeObject(this);
         }
+    }
+    
+    public void setSpeed(int spd)
+    {
+        speed = spd;
     }
 }
--- TitleScreen.java
+++ TitleScreen.java
@@ -18,10 +18,11 @@ public class TitleScreen extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
-        
+
         addObject(titleLabel, getWidth()/2, getHeight()/2);
+        prepare();
     }
-    
+
     public void act()
     {
         if(Greenfoot.isKeyDown("space"))
@@ -29,5 +30,25 @@ public class TitleScreen extends World
             MyWorld gameWorld = new MyWorld();
             Greenfoot.setWorld(gameWorld);
         }
+    }
+    
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        Dolphin dolphin = new Dolphin();
+        addObject(dolphin,463,110);
+        dolphin.setLocation(464,83);
+        dolphin.setLocation(480,75);
+        Label label = new Label("Press <space> to Start", 40);
+        addObject(label,160,251);
+        label.setLocation(284,311);
+        label.setLocation(297,330);
+        Label label2 = new Label("Hold <A> and <D> to Move", 40);
+        addObject(label2,231,220);
+        label2.setLocation(309,252);
+        dolphin.setLocation(480,106);
     }
 }
